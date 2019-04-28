00411167b456c2b3d7a8ca16f38f73f38ab5ed03
Victor0118 <547106693@qq.com>
2019-04-28 16:38:57 -0400

    fix conflict

--- src/main/python/msmarco/retrieve.py
+++ src/main/python/msmarco/retrieve.py
@@ -12,31 +12,43 @@ from jnius import autoclass
 JString = autoclass('java.lang.String')
 JSearcher = autoclass('io.anserini.search.SimpleSearcher')
 
-FLAGS = flags.FLAGS
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Retrieve MS MARCO Passages')
+    parser.add_argument('--qid_queries', required=True, default='', help='query id - query mapping file')
+    parser.add_argument('--output', required=True, default='', help='output filee')
+    parser.add_argument('--index', required=True, default='', help='index path')
+    parser.add_argument('--hits', default=10, help='number of hits to retrieve')
+    parser.add_argument('--b1', default=0.6, help='BM25 b1 parameter')
+    parser.add_argument('--k', default=0.8, help='BM25 k parameter')
+    parser.add_argument('--rm3', action='store_true', default=False, help='BM25 k parameter')
+    parser.add_argument('--fbTerms', default=10, type=int, help='RM3 parameter: number of expansion terms')
+    parser.add_argument('--fbDocs', default=10, type=int, help='RM3 parameter: number of documents')
+    parser.add_argument('--originalQueryWeight', default=0.5, type=float, help='RM3 parameter: weight to assign to the original query')
+    # Tuning on dev set obtains b1=0.6, k=0.8 w/ MRR@10 = 0.1906588552326375
+    # Note that this differs slightly from the value reported in https://arxiv.org/abs/1904.08375
+    # which uses the Anserini default of b1=0.9, k=0.4 w/ MRR@10 = 0.18388092964024202
+    # To reperform tuning, see tune_bm25.py
 
-flags.DEFINE_string("qid_queries", None, "query id - query mapping file.")
-flags.DEFINE_string("output", None, "File to write the retrieved docs.")
-flags.DEFINE_string("index", None, "Path to the Anserini Index.")
-flags.DEFINE_integer("hits", 10, "File containing the topics.")
+    args = parser.parse_args()
 
-FLAGS(sys.argv)
-
-searcher = JSearcher(JString(FLAGS.index))
-searcher.setBM25Similarity(0.9, 0.4)
-
-with open(FLAGS.output, "w") as fout:
-  start_time = time.time()
-  for line_number, line in enumerate(open(FLAGS.qid_queries)):
-    qid, query = line.strip().split("\t")
-    hits = searcher.search(JString(query.encode("utf8")), FLAGS.hits)
-    if line_number % 10 == 0:
-      time_per_query = (time.time() - start_time) / (line_number + 1)
-      print("Retrieving query {} ({:0.3f} s/query)".format(
-          line_number, time_per_query))
-    for rank in range(len(hits)):
-      docno = hits[rank].docid
-      fout.write("{}\t{}\t{}\n".format(qid, docno, rank + 1))
-
-print('Done!')
+    searcher = JSearcher(JString(args.index))
+    searcher.setBM25Similarity(float(args.b1), float(args.k))
+    print('Initializing BM25, setting b1={} and k={}'.format(args.b1, args.k))
+    if args.rm3:
+        searcher.setRM3Reranker(args.fbTerms, args.fbDocs, args.originalQueryWeight)
+        print('Initializing RM3, setting fbTerms={}, fbDocs={} and originalQueryWeight={}'.format(args.fbTerms, args.fbDocs, args.originalQueryWeight))
 
 
+    with open(args.output, 'w') as fout:
+      start_time = time.time()
+      for line_number, line in enumerate(open(args.qid_queries)):
+          qid, query = line.strip().split('\t')
+          hits = searcher.search(JString(query.encode('utf8')), int(args.hits))
+          if line_number % 10 == 0:
+              time_per_query = (time.time() - start_time) / (line_number + 1)
+              print('Retrieving query {} ({:0.3f} s/query)'.format(line_number, time_per_query))
+          for rank in range(len(hits)):
+              docno = hits[rank].docid
+              fout.write('{}\t{}\t{}\n'.format(qid, docno, rank + 1))
+
+    print('Done!')
